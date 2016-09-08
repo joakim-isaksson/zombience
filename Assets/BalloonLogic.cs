82abcf6acb75838d94572ed1693adba16964b809
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2016-09-08 21:15:39 +0300

    stuff

---
+++ Assets/BalloonLogic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonLogic : MonoBehaviour
+{
+	public float DestroyOnAltitude;
+
+	private Rigidbody2D rb;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		rb.constraints = RigidbodyConstraints2D.FreezeAll;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (transform.position.y > DestroyOnAltitude) Destroy(gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag.Equals("Player"))
+		{
+			rb.constraints = RigidbodyConstraints2D.None;
+		}
+	}
+}
