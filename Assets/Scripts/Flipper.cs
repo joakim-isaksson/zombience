594b2b3e9b15d4242a87be94c187b0a9a6f458d3
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2016-09-09 12:44:33 +0300

    party zombies

---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flipper : MonoBehaviour
+{
+
+	private GameObject follow = null;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (follow == null) return;
+		if (follow.transform.position.x < transform.position.x)
+		{
+			Vector3 theScale = transform.localScale;
+			theScale.x = -Mathf.Abs(theScale.x);
+			transform.localScale = theScale;
+		}
+		else
+		{
+			Vector3 theScale = transform.localScale;
+			theScale.x = Mathf.Abs(theScale.x);
+			transform.localScale = theScale;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag.Equals("Player")) follow = other.gameObject;
+	}
+}
