7179c30f4981ee5290422edcba528f194b206c1a
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2016-09-09 14:58:02 +0300

    more final touch

---
+++ Assets/Scripts/AnyKeyToStart.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class AnyKeyToStart : MonoBehaviour
+{
+
+	private bool started = false;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButtonDown("Cancel"))
+		{
+			Application.Quit();
+		}
+
+		if (!started && Input.anyKeyDown)
+		{
+			started = true;
+			SceneManager.LoadScene("Level1");
+		}
+	}
+}
---
+++ Assets/Scripts/TheEnd.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TheEnd : MonoBehaviour
+{
+	public float EndDelay = 10.0f;
+	public GameObject Credits;
+
+	private bool gameEnded = false;
+
+	// Use this for initialization
+	void Start()
+	{
+		Credits.SetActive(false);
+	}
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.tag.Equals("Player"))
+		{
+			Credits.SetActive(true);
+			if (!gameEnded)
+			{
+				gameEnded = true;
+				StartCoroutine(EndGameWithDelay(EndDelay));
+			}
+		}
+	}
+
+	IEnumerator EndGameWithDelay(float time)
+	{
+		yield return new WaitForSeconds(time);
+		SceneManager.LoadScene("StartScreen");
+	}
+}
